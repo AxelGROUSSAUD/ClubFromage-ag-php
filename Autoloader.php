40a3d3f51858e9889b1a7cd867679bbe3bcf8dc5
Axel Groussaud <axel.groussaud@saintmichelannecy.fr>
2020-10-07 11:39:43 +0200

    classe autloader déplacer et test hyfratation fini

---
+++ Autoloader.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Club_Fromage\Model;
+class Autoloader
+{
+   static function register(){
+       spl_autoload_register(function($class_name){
+       $file = str_replace('\\', '/', $class_name);
+       $file = '.' . $file . '.php';
+       $file = str_replace('Club_Fromage','',$file);
+       if(file_exists($file))
+           return require $file;
+       });
+   }
+
+   /*
+    *
+    *static function register() {
+    *
+    * spl_autoload_register(array(__CLASS__, 'autoload'));
+    *
+    * }
+    *
+    * static function autoload($class_name){
+
+       $file = str_replace('\\', '/', $class_name);
+       $file = '.' . $file . '.php';
+       $file = str_replace('Club_Fromage','',$file);
+       if(file_exists($file))
+           return require $file;
+       
+   }*/
+
+}
